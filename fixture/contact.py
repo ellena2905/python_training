056417bd0d00800e100a44ac65ba545677045eb6
ellena2905 <yelena_2905@mail.ru>
2016-04-10 21:05:32 +0300

    Добавление теста по изменению контакта

--- fixture/contact.py
+++ fixture/contact.py
@@ -81,7 +81,7 @@ class ContactHelper:
         wd.find_element_by_name("notes").click()
         wd.find_element_by_name("notes").clear()
         wd.find_element_by_name("notes").send_keys(contact_info.notes)
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+        wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
 
 
     def delete_first_contact(self):
@@ -93,3 +93,86 @@ class ContactHelper:
         wd.find_element_by_id("container").click()
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
+
+
+
+    def edit_first_contact(self, contact_info):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home").click()
+        #select first contact
+        wd.find_element_by_name("selected[]").click()
+        #start editing
+        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[8]/a/img").click()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact_info.firstname)
+        wd.find_element_by_name("middlename").click()
+        wd.find_element_by_name("middlename").clear()
+        wd.find_element_by_name("middlename").send_keys(contact_info.middlename)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact_info.lastname)
+        wd.find_element_by_name("nickname").clear()
+        wd.find_element_by_name("nickname").click()
+        wd.find_element_by_name("nickname").send_keys(contact_info.nickname)
+        wd.find_element_by_name("title").click()
+        wd.find_element_by_name("title").clear()
+        wd.find_element_by_name("title").send_keys(contact_info.title)
+        wd.find_element_by_name("company").click()
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact_info.company_name)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact_info.company_address)
+        wd.find_element_by_name("home").click()
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact_info.company_home_num)
+        wd.find_element_by_name("mobile").click()
+        wd.find_element_by_name("mobile").clear()
+        wd.find_element_by_name("mobile").send_keys(contact_info.company_home_phone)
+        wd.find_element_by_name("work").click()
+        wd.find_element_by_name("work").clear()
+        wd.find_element_by_name("work").send_keys(contact_info.add_company_name)
+        wd.find_element_by_name("fax").click()
+        wd.find_element_by_name("fax").clear()
+        wd.find_element_by_name("fax").send_keys(contact_info.company_fax)
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").click()
+        wd.find_element_by_name("email").clear()
+        wd.find_element_by_name("email").send_keys(contact_info.email1)
+        wd.find_element_by_name("email2").click()
+        wd.find_element_by_name("email2").clear()
+        wd.find_element_by_name("email2").send_keys(contact_info.email2)
+        wd.find_element_by_name("email3").click()
+        wd.find_element_by_name("email3").clear()
+        wd.find_element_by_name("email3").send_keys(contact_info.email3)
+        wd.find_element_by_name("homepage").click()
+        wd.find_element_by_name("homepage").clear()
+        wd.find_element_by_name("homepage").send_keys(contact_info.company_site)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[31]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[31]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[7]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[7]").click()
+        wd.find_element_by_name("byear").click()
+        wd.find_element_by_name("byear").clear()
+        wd.find_element_by_name("byear").send_keys(contact_info.year_of_birth)
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[31]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[3]//option[31]").click()
+        if not wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[7]").is_selected():
+            wd.find_element_by_xpath("//div[@id='content']/form/select[4]//option[7]").click()
+        wd.find_element_by_name("ayear").click()
+        wd.find_element_by_name("ayear").clear()
+        wd.find_element_by_name("ayear").send_keys(contact_info.anniversary_year)
+        wd.find_element_by_name("address2").click()
+        wd.find_element_by_name("address2").clear()
+        wd.find_element_by_name("address2").send_keys(contact_info.add_address)
+        wd.find_element_by_name("phone2").click()
+        wd.find_element_by_name("phone2").clear()
+        wd.find_element_by_name("phone2").send_keys(contact_info.add_phone)
+        wd.find_element_by_name("notes").click()
+        wd.find_element_by_name("notes").clear()
+        wd.find_element_by_name("notes").send_keys(contact_info.notes)
+        #submit editing
+        wd.find_element_by_xpath("//div[@id='content']/form[1]/input[22]").click()
+
+
---
+++ test/test_edit_contact.py
@@ -0,0 +1,7 @@
+from model.contact_info import Contact_info
+
+def test_edit_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact_info(firstname="2", middlename="2", lastname="2", nickname="2", title="2", company_name="2", company_address="1", company_home_num="1", company_home_phone="1", add_company_name="1", company_fax="1", email1="1", email2="1", email3="1", company_site="1", year_of_birth="1", anniversary_year="1", add_address="1", add_phone="1",
+                                 notes="987654"))
+    app.session.logout()
